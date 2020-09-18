de73b86c3f8d6a42c1946b5b569123376342aa05
Bernard Gresola <btgresola@uerm.edu.ph>
2020-09-18 12:33:56 +0800

    first commit

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f18a84b4d6ba3d85ea5aebe5713fbf89.js"
+  "/precache-manifest.721b8a969134f8915dfd674c5abcbe07.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "etriage-dashboard"});
